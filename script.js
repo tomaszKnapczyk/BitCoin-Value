d269f85f23d74d4317426ee235da9e2df02561c6
Tomasz Knapczyk <37016058+tomaszKnapczyk@users.noreply.github.com>
2018-06-04 20:06:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 $(function(){
-  checkValue();
-  setInterval(checkValue, 10000);
-  
-  function checkValue(){
     $.getJSON("https://api.coindesk.com/v1/bpi/currentprice.json")
     .done(function(data){
       $(".priceEUR").text(data.bpi.EUR.rate);
@@ -11,5 +7,4 @@ $(function(){
     .fail(function(){
       console.log("something went wrong!");
     });
-  }
 });
